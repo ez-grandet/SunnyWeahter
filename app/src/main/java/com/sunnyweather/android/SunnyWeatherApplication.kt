33a00aff4b767bdcd07197f8cf4d008ac23ed7b8
ez-grandet <3440370776@qq.com>
2021-01-04 16:09:09 +0800

    First commit.

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -8,7 +8,7 @@ class SunnyWeatherApplication : Application() {
 
     companion object {
 
-        const val TOKEN = "" // 填入你申请到的令牌值
+        const val TOKEN = "mdM7Z9i2V0823fAg" // 填入你申请到的令牌值
 
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
